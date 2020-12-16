995273c4c4d83c4392fe81bb7713ae4c478fdb1a
rago89 <rago89@gmail.com>
2020-12-16 17:20:57 +0100

    add handler add-items and listener add-items

---
+++ src/handlers/add-items.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// A handler that grab the users input and store in the data in variable items
+
+export  function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+      text,
+      done: false
+    };
+
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+  }
