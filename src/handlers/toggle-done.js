02b74e01525bf39204c6a5550f2d0c995f3eab5c
DIVYA SREE <67485922+DIVYASREE345@users.noreply.github.com>
2020-12-17 12:40:53 +0100

    Update toggle-done.js

--- src/handlers/toggle-done.js
+++ src/handlers/toggle-done.js
@@ -1,6 +1,8 @@
 'use strict';
 console.log('--- loading handler: toggle.js');
 //after refreshing data is there.
+import { items ,itemsList } from '../data.js';
+    
 
 export function toggleDone(e) {
     if (!e.target.matches('input')) return; // skip this unless it's an input
